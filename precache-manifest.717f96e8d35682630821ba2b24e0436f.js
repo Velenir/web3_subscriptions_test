5a2e90935b73d6b3baf81c02a985117c2eed04b1
Velenir <velenir@gmail.com>
2020-10-16 14:19:15 +0300

    Updates

---
+++ precache-manifest.717f96e8d35682630821ba2b24e0436f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96a871d3c306c770cd596effeb34ae26",
+    "url": "/web3_subscriptions_test/index.html"
+  },
+  {
+    "revision": "c2fd8d1840a24d7dd9bf",
+    "url": "/web3_subscriptions_test/static/css/main.cd96e512.chunk.css"
+  },
+  {
+    "revision": "10bdf1dd01135da68b21",
+    "url": "/web3_subscriptions_test/static/js/2.a227e2d5.chunk.js"
+  },
+  {
+    "revision": "5034144940501b7d88341244d8b00679",
+    "url": "/web3_subscriptions_test/static/js/2.a227e2d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2fd8d1840a24d7dd9bf",
+    "url": "/web3_subscriptions_test/static/js/main.f084dacc.chunk.js"
+  },
+  {
+    "revision": "b3ecb1dafba73b749fb6",
+    "url": "/web3_subscriptions_test/static/js/runtime-main.8a65539c.js"
+  }
+]);
